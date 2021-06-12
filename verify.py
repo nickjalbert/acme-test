d130b52560e1ce2a44192dd21b40e562d543e63f
Nick Jalbert <nick@jalbert.io>
2021-06-12 18:07:34 -0400

    Add tensorflow check to verify

--- verify.py
+++ verify.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import sonnet as snt
 
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
 print("TensorFlow version {}".format(tf.__version__))
 print("Sonnet version {}".format(snt.__version__))
