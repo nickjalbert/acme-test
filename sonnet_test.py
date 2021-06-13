859f49af692e7128d17a3d2a09c3bd46a152d441
Nick Jalbert <nick@jalbert.io>
2021-06-13 14:24:21 +0000

    Formatting

--- sonnet_test.py
+++ sonnet_test.py
@@ -4,13 +4,16 @@ import sonnet as snt
 batch_size = 10
 input_size = 30
 
-mlp = snt.Sequential([
-    snt.Linear(1024),
-    tf.nn.relu,
-    snt.Linear(20),
-])
+mlp = snt.Sequential(
+    [
+        snt.Linear(1024),
+        tf.nn.relu,
+        snt.Linear(20),
+    ]
+)
 
 logits = mlp(tf.random.normal([batch_size, input_size]))
 
 import pdb
+
 pdb.set_trace()
