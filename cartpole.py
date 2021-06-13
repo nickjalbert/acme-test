c7e3e68fa2de9ff29d3be49cea60b4a70c5cee4d
Nick Jalbert <nick@jalbert.io>
2021-06-13 14:23:55 +0000

    Formatting

--- cartpole.py
+++ cartpole.py
@@ -17,9 +17,9 @@ import imageio
 import base64
 
 # Set up a virtual display for rendering.
-#display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
+# display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
 
-environment = gym.make('CartPole-v1')
+environment = gym.make("CartPole-v1")
 environment = wrappers.GymWrapper(environment)  # To dm_env interface.
 environment = wrappers.SinglePrecisionWrapper(environment)
 
@@ -32,26 +32,28 @@ num_dimensions = np.prod(environment_spec.actions.shape, dtype=int)
 # BasicRNN, taken from r2d2 test
 # https://github.com/deepmind/acme/blob/master/acme/agents/tf/r2d2/agent_test.py
 class BasicRNN(networks.RNNCore):
+    def __init__(self, action_spec: specs.DiscreteArray):
+        super().__init__(name="basic_r2d2_RNN_network")
+        self._net = snt.DeepRNN(
+            [
+                snt.Flatten(),
+                snt.LSTM(20),
+                snt.nets.MLP([50, 50, action_spec.num_values]),
+            ]
+        )
 
-  def __init__(self, action_spec: specs.DiscreteArray):
-    super().__init__(name='basic_r2d2_RNN_network')
-    self._net = snt.DeepRNN([
-        snt.Flatten(),
-        snt.LSTM(20),
-        snt.nets.MLP([50, 50, action_spec.num_values])
-    ])
+    def __call__(self, inputs, state):
+        return self._net(inputs, state)
 
-  def __call__(self, inputs, state):
-    return self._net(inputs, state)
+    def initial_state(self, batch_size: int, **kwargs):
+        return self._net.initial_state(batch_size)
 
-  def initial_state(self, batch_size: int, **kwargs):
-    return self._net.initial_state(batch_size)
+    def unroll(self, inputs, state, sequence_length):
+        return snt.static_unroll(self._net, inputs, state, sequence_length)
 
-  def unroll(self, inputs, state, sequence_length):
-    return snt.static_unroll(self._net, inputs, state, sequence_length)
 
-agent_logger = loggers.TerminalLogger(label='agent', time_delta=10.)
-env_loop_logger = loggers.TerminalLogger(label='env_loop', time_delta=10.)
+agent_logger = loggers.TerminalLogger(label="agent", time_delta=10.0)
+env_loop_logger = loggers.TerminalLogger(label="env_loop", time_delta=10.0)
 
 # Create the D4PG agent.
 agent = r2d2.R2D2(
@@ -61,17 +63,20 @@ agent = r2d2.R2D2(
     trace_length=50,
     replay_period=61,
     logger=agent_logger,
-    checkpoint=False
+    checkpoint=False,
 )
 
 # Create an loop connecting this agent to the environment created above.
 env_loop = environment_loop.EnvironmentLoop(
-    environment, agent, logger=env_loop_logger, should_update=True)
+    environment, agent, logger=env_loop_logger, should_update=True
+)
 
 while True:
     trial_count = 10
     learning_run_count = 100
     trials = [env_loop.run_episode() for i in range(trial_count)]
-    total_steps = sum([t['episode_length'] for t in trials])
-    print(f'{total_steps/len(trials):.2f} after {trials[-1]["episodes"]} episodes')
+    total_steps = sum([t["episode_length"] for t in trials])
+    print(
+        f'{total_steps/len(trials):.2f} after {trials[-1]["episodes"]} episodes'
+    )
     env_loop.run(num_episodes=learning_run_count)
